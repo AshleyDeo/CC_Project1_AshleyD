24a15522f96aa25e0dfadc73feab182cfb0a8e34
AshleyDeo <35739765+AshleyDeo@users.noreply.github.com>
2020-03-09 13:22:26 -0400

    Added Fireworks

--- script.js
+++ script.js
@@ -1,114 +1,333 @@
 //------CLASSES----------------------------------
-class Something {
-    constructor(x, y, sx, f) {
-        this.posx = x;
-        this.posy = y;
-        this.scalex = sx;
-        this.scaley = 100;
-        this.dirx = 10;
-        this.diry = 1;
-        this.flip = f;
-
+class Firework_1 {
+    constructor() {
+        this.xpos = 0;
+        this.ypos = 0;
+        this.rad = 10; //radius
+        this.run = false; //bool for update to run contents
+        this.blow = false; //bool for explosion
+        this.splodex = 20; //position x for explosion particles
+        this.splodeSpd = 1; //speed of explosion particles
+        //this.alpha = 255;
     }
 
     update() {
-        fill(255);
-        noStroke();
-        ellipse(this.posx, this.posy, this.scalex, this.scaley);
-        /*this.posx += this.dirx;
-        if (this.posx >= windowWidth - 20 || this.posx <= 0) {
-            this.dirx *= -1;
-        }*/
-        this.scalex += this.flip;
-        if (this.scalex >= 100 || this.scalex <= 0) {
-            this.flip *= -1;
+        if (this.run) { //if mouse clicked run explosion
+            this.grow();
         }
     }
-}
-class Something_2 {
-    constructor(x, y, a, sp, c) {
-        this.f_color = c;
-        this.posx = x;
-        this.posy = y;
-        this.scalex = 100;
-        this.scaley = 100;
-        this.dirx = 10;
-        this.diry = 1;
-        this.flip = 1;
-        this.dist = 40;
-        this.angl = a;
-        this.spd = sp;
-        this.angle_accum = a;
-    }
 
-    update() {
-        stroke(this.f_color);
-        strokeWeight(3);
+    grow() { //grows explosion particle
         push();
-        translate(this.posx, this.posy);
-        angleMode(DEGREES);
-        rotate(this.angl);
-        line(-this.dist / 2, -this.dist / 2, this.dist / 2, this.dist / 2);
-        this.angl += this.spd;
-        this.angle_accum += 5;
-        if (this.angl % 170 == 0 || this.angl % 270 == 0) {
-            this.spd *= -1;
-            this.angle_accum = 0;
+        //console.log(this.blow);
+        translate(this.xpos, this.ypos); //center at mouse
+        if (this.blow == false) { 
+            fill(255,200,100);
+            stroke(200,100,100);
+            strokeWeight(3);
+            ellipse(0, 0, this.rad);
+            this.rad += 10;
+            if (this.rad > 50) {
+                this.blow = true;
+                this.rad = 10;
+            }
+        } else {
+            this.explode();
         }
         pop();
     }
+    explode() {
+        if (this.splodex < 400) {
+            for (let i = 0; i < 8; i++) {
+                for (let j = 0; j < 8; j++) {
+                    push();
+                    rotate(degrees(i * 15 + j));
+                    noStroke();
+                    strokeWeight(2);
+                    fill(255, 200, 200);
+                    ellipse(this.splodex, this.splodex / 2, 10);
+                    pop();
+                    
+                }
+                this.splodex++;
+            }
+        } else {
+            this.run = false; //stop running and reset vars
+            this.blow = false;
+            this.splodex = 20;
+        }
+    }
 }
 
+class Firework_2 {
+    constructor(x,y,ys,n) {
+        this.xpos = x;
+        this.ypos = y;
+        this.yStop = ys; // position y to stop at
+        this.rad = 10; //radius
+        this.blow = false;
+        this.splodex = 20;  //particle position x
+        this.splodey = 20; //particle position y
+        this.splodeSpd = 2; //explosion particle speed
+        //this.falling = false;
+        this.splodeNum = n; //particle type
+    }
+
+    update() {
+        this.grow();
+    }
+
+    grow() {
+        push();
+        translate(this.xpos, this.ypos);
+        if (this.ypos > this.yStop) {
+            fill(random(150, 255),random(50,150),random(0,100));
+            noStroke();
+            ellipse(0, 0, this.rad);
+            this.ypos -= 10;
+            this.rad += 0.5;
+        } else {
+            this.rad = 10;
+            this.explode();
+        }
+        pop();
+        /*if (this.falling) {
+            this.fall();
+        }*/
+    }
+    explode() {
+        if (this.splodex < 300) {
+            if (this.splodeNum == 0) {
+                for (let i = 0; i < 8; i++) {
+                    push();
+                    rotate(degrees(i * 20));
+                    stroke(255, 0, 0);
+                    strokeWeight(2);
+                    fill(255, 200, 200);
+                    rect(this.splodex/2, this.splodey/2, 10);
+                    pop();
+                    this.splodex += this.splodeSpd;
+                    this.splodey += this.splodeSpd;
+                }
+            } else if (this.splodeNum == 1) {
+                for (let i = 0; i < 6; i++) {
+                    push();
+                    rotate(degrees(i * 35));
+                    stroke(200, 100, 200);
+                    strokeWeight(2);
+                    fill(100, 100, 255);
+                    ellipse(this.splodex, this.splodex, 60);
+                    pop();
+            
+                    push();
+                    rotate(degrees(i * 40));
+                    stroke(200, 100, 200);
+                    strokeWeight(2);
+                    fill(200, 0, 100);
+                    rect(this.splodex, this.splodex, 30);
+                    pop();
+                    this.splodex += this.splodeSpd;
+                    this.splodey += this.splodeSpd;
+                }
+            } else if (this.splodeNum == 2) {
+                for (let i = 0; i < 7; i++) {
+                    push();
+                    rotate(degrees(i * 30));
+                    stroke(100, 100, 100);
+                    strokeWeight(2);
+                    fill(100, 255, 100);
+                    rect(this.splodex - 20, this.splodey - 10, 30);
+                    pop();
+                    this.splodex++;
+                    this.splodey++;
+                }
+            }
+        } else if (this.splodey < 400) {
+            if (this.splodeNum == 0) {
+                for (let i = 0; i < 8; i++) {
+                    push();
+                    rotate(degrees(i * 20));
+                    stroke(255, 0, 0);
+                    strokeWeight(2);
+                    fill(255, 200, 200);
+                    rect(this.splodex, this.splodey, 10);
+                    pop();
+                    this.splodey += this.splodeSpd;
+                }
+            } else if (this.splodeNum == 1) {
+                for (let i = 0; i < 6; i++) {
+                    push();
+                    rotate(degrees(i * 35));
+                    stroke(200, 100, 200);
+                    strokeWeight(2);
+                    fill(100, 100, 255);
+                    ellipse(this.splodex, this.splodey, 60);
+                    pop();
+                    this.splodex += this.splodeSpd;
+                    this.splodey += this.splodeSpd;
+                }
+            } else if (this.splodeNum == 2) {
+                for (let i = 0; i < 7; i++) {
+                    push();
+                    rotate(degrees(i * 30));
+                    stroke(100, 100, 100);
+                    strokeWeight(2);
+                    fill(100, 255, 100, 255/2);
+                    rect(this.splodex - 20, this.splodey - 10, 20);
+                    pop();
+                    this.splodex += 1;
+                    this.splodey += 1;
+                }
+            }
+        } else {
+            this.splodex = 20;
+            this.splodey = 20;
+            this.splodeNum = Math.floor(Math.random() * 3);
+            this.xpos = random(20, width - 20);
+            this.ypos = height;
+            this.yStop = random(20, height / 2)
+        }
+    }
+
+    fall() {
+        if (this.fallNum < 60) {
+            console.log(this.ypos);
+            push();
+            translate(this.xpos, this.ypos);
+            for (let i = 0; i < 7; i++) {
+                rotate(degrees(i * 30));
+                stroke(100, 100, 100);
+                strokeWeight(2);
+                fill(100, 255, 100);
+                ellipse(this.splodex - 20, this.splodex - 10, 30);
+            }
+            pop();
+            this.fallNum++;
+            this.ypos += 5;
+        } else {
+            this.splodex = 20;
+            this.splodeNum = Math.floor(Math.random() * 3);
+            this.fallNum = 0;
+            this.falling = false;
+            this.xpos = random(20, width - 20);
+            this.ypos = height;
+            this.yStop = random(20, height / 2)
+        }
+    }
+}
+
+class ShootingStar {
+    consrtuctor(x,y,xs,ys,r) {
+        this.xpos = x;
+        this.ypos = y;
+        this.xspd = xs;
+        this.yspd = ys;
+        this.rad = r;
+        this.run = false;
+        this.blow = false;
+        this.splodex = 20;
+    }
+    update() {
+        if (this.run) {
+            this.grow();
+        } else {
+            console.log(this.xpos);
+            this.xpos += this.xs;
+            this.ypos += this.ys;
+            fill(255, 200, 100);
+            noStroke();
+            ellipse(this.xpos, this.ypos, this.rad);
+        }
+    }
+
+    grow() {
+        push();
+        //console.log(this.blow);
+        translate(this.xpos, this.ypos);
+        if (this.blow == false) {
+            fill(255, 200, 100);
+            noStroke();
+            ellipse(0, 0, this.rad);
+            this.rad += 10;
+            if (this.rad > 50) {
+                this.blow = true;
+                this.rad = 10;
+            }
+        } else {
+            this.explode();
+        }
+        pop();
+    }
+    explode() {
+        if (this.splodex < 200) {
+            for (let i = 0; i < 8; i++) {
+                push();
+                rotate(degrees(i * 20));
+                stroke(255, 0, 0);
+                strokeWeight(2);
+                fill(255, 200, 200);
+                rect(this.splodex, this.splodex, 10);
+                pop();
+                this.splodex++;
+            }
+        } else {
+            this.run = false;
+            this.blow = false;
+            this.splodex = 20;
+        }
+    }
+}
 
 //------GLOBAL VARS---------------------------------
-let obj_1 = new Something(100, 100, 0, 5);
-let obj_2 = new Something(400, 100, 90, -5);
-//let obj_3 = new Something_2(500, 500);
-let spins_1 = [];
-let spins_2 = [];
+let mouseFires = [5];
+let mouseIndex = 0;
+let fires = [10]
+let star_1;
 
 //------ FUNCTIONS-----------------------------------
 function setup() {
     let canvas = createCanvas(windowWidth - 20, windowHeight - 20);
-    let angle = 45;
-    for (let j = 0; j < 30; j++) {
-        for (let i = 0; i < 15; i++) {
-           /* if (j % 25 == 0 && i % 5 == 2) {
-                spins_1.push(new Something_2(30 + (50 * j), -17 + (50 * i), i, 7, color(255, 150, 150)));
-            } else if (j%13 == 2 && i % 3 == 0) {
-                spins_1.push(new Something_2(15 + (50 * j), 15 + (30 * i), i, -13, color(150, 150, 255)));
-            } else {
-                spins_1.push(new Something_2(30 + (50 * j), 30 + (50 * i), angle, 5, color(255)));
-            }*/
-            spins_1.push(new Something_2(30 + (50 * j), 30 + (50 * i), angle, 5, color(255)));
-            if (angle >= 360) {
-                angle -= 360;
-            }
-            angle += 7;
-        }
-        angle -= 7 * 14;
+
+    for (let i = 0; i < 5; i++) {
+        mouseFires[i] = new Firework_1();
     }
-    /*angle = 45;
-    for (let j = 0; j < 5; j++) {
-        for (let i = 0; i < 15; i++) {
-            spins_2.push(new Something_2(780 + (50 * j), 30 + (50 * i), angle, 3));
-            angle += 10;
-        }
-    }*/
+    for (let i = 0; i < 10; i++) {
+        fires[i] = new Firework_2(random(20, width - 20), height, random(20, height * 3 / 4), Math.floor(Math.random() * 3));
+    }
+
+    star_1 = new ShootingStar(random(20, width-20), random(20, height - 20), 0, 0, random(20,30))
 }
 
 function draw() {
-    background(0);
-    //obj_1.update();
-    //obj_2.update();
-    for (let i = 0; i < spins_1.length; i++) {
-        spins_1[i].update();
+    background(0,0,70);
+    for (let i = 0; i < 5; i++) {
+        mouseFires[i].update();
     }
-    for (let i = 0; i < spins_2.length; i++) {
-        spins_2[i].update();
+    for (let i = 0; i < 10; i++) {
+        fires[i].update();
     }
 }
 
 function windowResized() {
     resizeCanvas(windowWidth - 20, windowHeight - 20);
 }
+
+function mouseClicked() {
+    mouseFires[mouseIndex].xpos = mouseX;
+    mouseFires[mouseIndex].ypos = mouseY;
+    mouseFires[mouseIndex].run = true;
+    mouseIndex++;
+    if (mouseIndex >= 5) {
+        mouseIndex = 0;
+    }
+}
+
+function mouseMoved() {
+    if (star_1.xpos + star_1.rad < mouseX && star_1.xpos - star_1.rad > mouseX) {
+        if (star_1.ypos + star_1.rad < mouseY && star_1.ypos - star_1.rad > mouseY) {
+            star_1.blow = true;
+        }
+    }
+}
+
+// Adjective: Explosive
